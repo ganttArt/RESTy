4f7895660fe2d621db97655394b3ca8c3bcc23da
Chris Gantt <gantt.art@gmail.com>
2021-05-24 16:16:22 -1000

    Basic functionality working.

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,17 +4,39 @@ import '../style/form.scss';
 class Form extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      httpMethod: '',
+      url: ''
+    }
   }
+
+  radioButtonOnChange = (event) => this.setState({ httpMethod: event.target.value })
+
+  urlOnChange = (event) => this.setState({ url: event.target.value })
 
   render() {
     return (
-      <form>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <>
+        <form>
+          <label>
+            Url:
+            <input type="text" name="url" onChange={this.urlOnChange}/>
+          </label>
+          <div onChange={this.radioButtonOnChange}>
+            <input type="radio" value="GET" name="httpMethod"/>GET
+            <input type="radio" value="POST" name="httpMethod" />POST
+            <input type="radio" value="PUT" name="httpMethod" />PUT
+            <input type="radio" value="DELETE" name="httpMethod" />DELETE
+          </div>
+        </form>
+        <section>
+          <ul>
+            <li>URL: {this.state.url}</li>
+            <li>HTTP Method: {this.state.httpMethod}</li>
+          </ul>
+        </section>
+      </>
     )
   }
 }
